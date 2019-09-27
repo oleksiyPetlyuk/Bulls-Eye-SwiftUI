296593c359ec82a335bb9ac787f2e52436ac1faf
Oleksiy Petlyuk <oleksiy.petlyuk94@gmail.com>
2019-09-27 21:45:13 +0200

    Initial Commit

---
+++ BullsEyeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEyeSwiftUI
+//
+//  Created by Oleksiy Petlyuk on 9/27/19.
+//  Copyright © 2019 Oleksiy Petlyuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
